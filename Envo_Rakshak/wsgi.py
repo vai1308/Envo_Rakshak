af5c8e92db92616a0d8902062d5033c4158c4567
Vaibhav Gautam <vaibhavgautam801@gmail.com>
2024-07-04 16:45:00 +0530

    Changed wsgi

--- Envo_Rakshak/wsgi.py
+++ Envo_Rakshak/wsgi.py
@@ -11,8 +11,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise import WhiteNoise
+
+from Envo_Rakshak import MyWSGIApp
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Envo_Rakshak.settings')
 
 application = get_wsgi_application()
 
 app = application
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root="/static/")
