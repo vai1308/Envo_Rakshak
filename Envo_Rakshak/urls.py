a47c9864c4efc596c734c6e758545c51b3c924d0
Vaibhav Gautam <vaibhavgautam801@gmail.com>
2024-07-04 10:58:15 +0530

    Changed Settings

--- Envo_Rakshak/urls.py
+++ Envo_Rakshak/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('GreenTrip.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
